95eab20ba794302bd3d9959e41d00b9cc654e2e5
Gabriel Leon Brugnolo de Souza <gabriel.souza@elotech.com.br>
2023-06-24 13:46:00 -0300

    Adicionando testes e alterando as classes de transportes

--- src/main/java/org/example/transportes/Veiculo.java
+++ src/main/java/org/example/transportes/Veiculo.java
@@ -1,5 +1,15 @@
 package org.example.transportes;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Builder
+@AllArgsConstructor
 public abstract class Veiculo {
-    Double valorFipe;
+    Double valorFipe = 0D;
+
+    public Double calcularValoFipe() {
+        Double result = 0D;
+        return result;
+    }
 }
---
+++ src/test/java/transportes/Carga.java
@@ -0,0 +1,17 @@
+package transportes;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Carga {
+
+    private Carga veiculoCarga = new Carga();
+
+    @Test
+    public void deveCalcularCorretamentoOValorDaFipeQuandoOVeiculoPossuirPortaoAutomatico(){
+        var result = 0D;
+        veiculoCarga.
+
+        Assertions.assertEquals(result, 0D);
+    }
+}
